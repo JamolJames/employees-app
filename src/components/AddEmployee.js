352dec158949b7788918cee9f8aee681df95966b
Barthelemy Peter <bartpeter@outlook.com>
2023-05-11 21:33:46 -0500

    refactoring add employees

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,219 +1,204 @@
-import { useState } from "react";
+import { useState } from "react"
 import {
-    Button,
-    TextField,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-} from "@mui/material";
-import GenderMenu from "./GenderMenu";
-import DepartmentMenu from "./DepartmentMenu";
-import { useNavigate } from "react-router-dom";
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material"
+import GenderMenu from "./GenderMenu"
+import DepartmentMenu from "./DepartmentMenu"
 
-export default function AddEmployee() {
-    const navigate = useNavigate();
-    const [empId, setEmpId] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [salary, setSalary] = useState("");
-    const [fromDate, setFromDate] = useState("");
-    const [endDate, setEndDate] = useState("");
-    const [role, setRole] = useState("");
-    const [post, setPost] = useState("");
-    const [dob, setDob] = useState("");
-    const [gender, setGender] = useState("");
-    const [dept, setDept] = useState("");
-    const [id, setId] = useState("");
+export default function AddEmployee({ open, handleClose, setIsLoading }) {
+  const [empId, setEmpId] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [salary, setSalary] = useState("")
+  const [fromDate, setFromDate] = useState("")
+  const [endDate, setEndDate] = useState("")
+  const [role, setRole] = useState("")
+  const [post, setPost] = useState("")
+  const [dob, setDob] = useState("")
+  const [gender, setGender] = useState("")
+  const [dept, setDept] = useState("")
+  const [id, setId] = useState("")
 
-    const handleClose = () => {
-        navigate("/home");
-    };
+  const getGender = (gender) => {
+    setGender(gender)
+  }
 
-    const getGender = (gender) => {
-        setGender(gender);
-    };
+  const getDepartment = (dept) => {
+    setDept(dept)
+  }
 
-    const getDepartment = (dept) => {
-        setDept(dept);
-    };
+  const getId = (id) => {
+    setId(id)
+  }
 
-    const getId = (id) => {
-        setId(id);
-    };
+  const handleSumbit = async (e) => {
+    setIsLoading(true)
+    const data = {
+      emp_id: empId,
+      first_name: firstName,
+      last_name: lastName,
+      salary: salary,
+      from_date: fromDate,
+      to_date: endDate,
+      role: role,
+      post: post,
+      dob: dob,
+      gender: gender,
+      dept_no: 53,
+    }
+    e.preventDefault()
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
 
-    const handleSumbit = async (e) => {
-        const data = {
-            emp_id: empId,
-            first_name: firstName,
-            last_name: lastName,
-            salary: salary,
-            from_date: fromDate,
-            to_date: endDate,
-            role: role,
-            post: post,
-            dob: dob,
-            gender: gender,
-            dept_no: id,
-        };
-        e.preventDefault();
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        };
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/api/employees`,
+        options
+      )
 
-        try {
-            const response = await fetch(
-                `${process.env.REACT_APP_BASE_URL}/api/employees`,
-                options
-            );
+      handleClose()
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
-            handleClose();
-        } catch (e) {
-            console.error(e);
-        }
-    };
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Add Employee</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="empId"
+          label="Employee ID"
+          type="string"
+          fullWidth
+          variant="standard"
+          onChange={(e) => {
+            setEmpId(e.target.value)
+          }}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="firstName"
+          label="First Name"
+          type="string"
+          fullWidth
+          variant="standard"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value)
+          }}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="lastName"
+          label="Last Name"
+          type="string"
+          fullWidth
+          variant="standard"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value)
+          }}
+        />
 
-    // Brings up Problems in console
-    // useEffect(() => {
-    //     const found = rows.find((employee) => employee.empId === empId);
-    //     if (found) {
-    //         setFirstName(found.firstName);
-    //         setLastName(found.lastName);
-    //         setDob(found.dob);
-    //         setGender(found.gender);
-    //     }
-    // }, [empId, rows]);
+        <label className="Form" style={{ color: "grey" }}>
+          DOB:
+        </label>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="dob"
+          type="date"
+          fullWidth
+          variant="standard"
+          value={dob}
+          onChange={(e) => {
+            setDob(e.target.value)
+          }}
+        />
+        <GenderMenu getGender={getGender} />
+        <DepartmentMenu getDepartment={getDepartment} getId={getId} />
 
-    return (
-        <Dialog open={true} onClose={handleClose}>
-            <DialogTitle>Add Employee</DialogTitle>
-            <DialogContent>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="empId"
-                    label="Employee ID"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => {
-                        setEmpId(e.target.value);
-                    }}
-                />
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="firstName"
-                    label="First Name"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={firstName}
-                    onChange={(e) => {
-                        setFirstName(e.target.value);
-                    }}
-                />
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="lastName"
-                    label="Last Name"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={lastName}
-                    onChange={(e) => {
-                        setLastName(e.target.value);
-                    }}
-                />
-                <label className="DOB" style={{ color: "grey" }}>
-                    DOB:
-                </label>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="dob"
-                    type="date"
-                    fullWidth
-                    variant="standard"
-                    value={dob}
-                    onChange={(e) => {
-                        setDob(e.target.value);
-                    }}
-                />
-                <GenderMenu getGender={getGender} />
-                <DepartmentMenu getDepartment={getDepartment} getId={getId} />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="role"
+          label="Role"
+          type="string"
+          fullWidth
+          variant="standard"
+          value={role}
+          onChange={(e) => {
+            setRole(e.target.value)
+          }}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="post"
+          label="Position"
+          type="string"
+          fullWidth
+          variant="standard"
+          value={post}
+          onChange={(e) => {
+            setPost(e.target.value)
+          }}
+        />
 
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="role"
-                    label="Role"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={role}
-                    onChange={(e) => {
-                        setRole(e.target.value);
-                    }}
-                />
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="post"
-                    label="Position"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={post}
-                    onChange={(e) => {
-                        setPost(e.target.value);
-                    }}
-                />
-
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="salary"
-                    label="Salary"
-                    type="number"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => setSalary(Number(e.target.value))}
-                />
-                <label className="Form" style={{ color: "grey" }}>
-                    From Date:
-                </label>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="fromDate"
-                    type="date"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => setFromDate(e.target.value)}
-                />
-                <label className="Form" style={{ color: "grey" }}>
-                    End Date:
-                </label>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="endDate"
-                    type="date"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => setEndDate(e.target.value)}
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={async (e) => await handleSumbit(e)}>Submit</Button>
-            </DialogActions>
-        </Dialog>
-    );
+        <TextField
+          autoFocus
+          margin="dense"
+          id="salary"
+          label="Salary"
+          fullWidth
+          variant="standard"
+          onChange={(e) => setSalary(Number(e.target.value))}
+        />
+        <label className="Form" style={{ color: "grey" }}>
+          From Date:
+        </label>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="fromDate"
+          fullWidth
+          variant="standard"
+          onChange={(e) => setFromDate(e.target.value)}
+        />
+        <label className="Form" style={{ color: "grey" }}>
+          End Date:
+        </label>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="endDate"
+          type="date"
+          fullWidth
+          variant="standard"
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={async (e) => await handleSumbit(e)}>Submit</Button>
+      </DialogActions>
+    </Dialog>
+  )
 }
--- src/components/AllEmployees.js
+++ src/components/AllEmployees.js
@@ -1,133 +1,143 @@
-import { useState, useEffect } from "react";
-import { Typography, Box, Container, IconButton } from "@mui/material";
+import { useState, useEffect } from "react"
+import { Typography, Box, Container, IconButton } from "@mui/material"
 import {
-    DataGrid,
-    GridToolbarQuickFilter,
-    GridToolbarContainer,
-    GridToolbarColumnsButton,
-    GridToolbarFilterButton,
-    GridToolbarExport,
-    GridToolbarDensitySelector,
-} from "@mui/x-data-grid";
-import AlertTrash from "./AlertTrash";
-import logo from "../img/Logo.svg";
-import { columns } from "../DummyData/data";
-import AddCircle from "@mui/icons-material/AddCircle";
-import { useNavigate } from "react-router-dom";
+  DataGrid,
+  GridToolbarQuickFilter,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+} from "@mui/x-data-grid"
+import AlertTrash from "./AlertTrash"
+import logo from "../img/Logo.svg"
+import { columns } from "../DummyData/data"
+import AddCircle from "@mui/icons-material/AddCircle"
+import { useNavigate } from "react-router-dom"
+import AddEmployee from "./AddEmployee"
 
 export default function AllEmployees() {
-    const navigate = useNavigate();
-    const [rowSelectionModel, setRowSelectionModel] = useState([]);
-    const [rows, setRows] = useState([]);
-    // const [del, setDel] = useState(true);
-    // const [rowSelectionModel, setRowSelectionModel] = useState([]);
-    // const addInfo = (nEntry) => {
-    //     const newEntry = {
-    //         id: "",
-    //         empId: nEntry.empId,
-    //         firstName: nEntry.firstName,
-    //         lastName: nEntry.lastName,
-    //         salary: nEntry.salary,
-    //         fromDate: nEntry.fromDate,
-    //         endDate: nEntry.endDate,
-    //         role: nEntry.role,
-    //         position: nEntry.position,
-    //     };
-    //     console.log(newEntry);
-    //     setRows([...rows, newEntry]);
-    // };
+  const navigate = useNavigate()
+  const [rowSelectionModel, setRowSelectionModel] = useState([])
+  const [rows, setRows] = useState([])
+  const [open, setOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-    const AddEmployeeIcon = () => (
-        <IconButton
-            size="small"
-            sx={{ color: "#8b0000" }}
-            onClick={() => navigate("/new-employee")}
-        >
-            <AddCircle />
-        </IconButton>
-    );
+  const handleClose = () => {
+    setOpen(false)
+  }
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_BASE_URL}/api/employees`)
-            .then((res) => res.json())
-            .then((data) =>
-                setRows(
-                    data.rows.map((employee) => ({
-                        id: employee.id,
-                        empId: employee.emp_id,
-                        lastName: employee.last_name,
-                        firstName: employee.first_name,
-                        age: employee.age,
-                        gender: employee.gender,
-                        salary: employee.salary,
-                        fromDate: employee.from_date,
-                        endDate: employee.to_date,
-                        post: employee.post,
-                        role: employee.role,
-                        dept: employee.dept_name,
-                    }))
-                )
-            );
-    }, []);
+  const AddEmployeeIcon = () => (
+    <IconButton
+      size="small"
+      sx={{ color: "#8b0000" }}
+      onClick={() => setOpen((prev) => !prev)}
+    >
+      <AddCircle />
+    </IconButton>
+  )
 
-    const deleteEmployees = () => {
-        setRows((prev) => prev.filter((employee) => !rowSelectionModel.includes(employee.id)));
-    };
+  useEffect(() => {
+    if (!isLoading)
+      fetch(`${process.env.REACT_APP_BASE_URL}/api/employees`)
+        .then((res) => res.json())
+        .then((data) =>
+          setRows(
+            data.rows.map(
+              // prettier-ignore
+              ({id, emp_id, last_name, first_name, age, gender, salary, from_date, to_date, post,role, dept_name,
+            }) => ({
+              id,
+              empId: emp_id,
+              lastName: last_name,
+              firstName: first_name,
+              age,
+              gender,
+              salary,
+              fromDate: from_date,
+              endDate: to_date,
+              post,
+              role,
+              dept: dept_name,
+            })
+            )
+          )
+        )
+  }, [isLoading])
 
-    function CustomToolbar() {
-        return (
-            <GridToolbarContainer>
-                <GridToolbarColumnsButton sx={{ color: "#8b0000" }} />
-                <GridToolbarDensitySelector sx={{ color: "#8b0000" }} />
-                <GridToolbarFilterButton sx={{ color: "#8b0000" }} />
-                <GridToolbarQuickFilter sx={{ color: "#8b0000" }} />
-                <GridToolbarExport sx={{ color: "#8b0000" }} />
-                <AddEmployeeIcon />
+  const deleteEmployees = () => {
+    setRows((prev) =>
+      prev.filter((employee) => !rowSelectionModel.includes(employee.id))
+    )
+  }
 
-                {/* Conditionally display Delete Button */}
-                {rowSelectionModel.length ? <AlertTrash confirmDelete={deleteEmployees} /> : null}
-            </GridToolbarContainer>
-        );
-    }
-
-    function CustomFilterPanel() {
-        return;
-    }
-    //Table Display
+  function CustomToolbar() {
     return (
-        <Container maxWidth="lg" sx={{ position: "relative", zIndex: "1" }}>
-            <Box sx={{ mt: 2 }}>
-                <img src={logo} className="Logo" alt="Coat of Arms Logo" width={100} height={100} />
-            </Box>
-            <Typography fontSize={30}>Short Term Employees</Typography>
-            <Box sx={{ height: 600, width: "100%", mt: 5, background: "#eceff6" }}>
-                <DataGrid
-                    slots={{
-                        toolbar: CustomToolbar,
-                    }}
-                    slotProps={{
-                        filterPanel: CustomFilterPanel,
-                    }}
-                    sx={{ boxShadow: 2 }}
-                    rows={rows}
-                    columns={columns}
-                    initialState={{
-                        pagination: {
-                            paginationModel: {
-                                pageSize: 8,
-                            },
-                        },
-                    }}
-                    pageSizeOptions={[8]}
-                    checkboxSelection
-                    disableRowSelectionOnClick
-                    onRowSelectionModelChange={(newRowSelectionModel) => {
-                        setRowSelectionModel(newRowSelectionModel);
-                        console.log(newRowSelectionModel);
-                    }}
-                    rowSelectionModel={rowSelectionModel}
-                />
-            </Box>
-        </Container>
-    );
+      <GridToolbarContainer>
+        <GridToolbarColumnsButton sx={{ color: "#8b0000" }} />
+        <GridToolbarDensitySelector sx={{ color: "#8b0000" }} />
+        <GridToolbarFilterButton sx={{ color: "#8b0000" }} />
+        <GridToolbarQuickFilter sx={{ color: "#8b0000" }} />
+        <GridToolbarExport sx={{ color: "#8b0000" }} />
+        <AddEmployeeIcon />
+
+        {/* Conditionally display Delete Button */}
+        {rowSelectionModel.length ? (
+          <AlertTrash confirmDelete={deleteEmployees} />
+        ) : null}
+      </GridToolbarContainer>
+    )
+  }
+
+  function CustomFilterPanel() {
+    return
+  }
+  //Table Display
+  return (
+    <Container maxWidth="lg" sx={{ position: "relative", zIndex: "1" }}>
+      <Box sx={{ mt: 2 }}>
+        <img
+          src={logo}
+          className="Logo"
+          alt="Coat of Arms Logo"
+          width={100}
+          height={100}
+        />
+      </Box>
+      <Typography fontSize={30}>Short Term Employees</Typography>
+      <Box sx={{ height: 600, width: "100%", mt: 5, background: "#eceff6" }}>
+        <DataGrid
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+          slotProps={{
+            filterPanel: CustomFilterPanel,
+          }}
+          sx={{ boxShadow: 2 }}
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 8,
+              },
+            },
+          }}
+          pageSizeOptions={[8]}
+          checkboxSelection
+          disableRowSelectionOnClick
+          onRowSelectionModelChange={(newRowSelectionModel) => {
+            setRowSelectionModel(newRowSelectionModel)
+            console.log(newRowSelectionModel)
+          }}
+          rowSelectionModel={rowSelectionModel}
+        />
+      </Box>
+      <AddEmployee
+        open={open}
+        handleClose={handleClose}
+        setIsLoading={setIsLoading}
+      />
+    </Container>
+  )
 }
