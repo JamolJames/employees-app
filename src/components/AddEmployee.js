c52674ca4fa9f8d02f32f04a0bc8e4538820eecc
Jamol James <jamolbjames@gmail.com>
2023-05-31 12:28:07 -0400

    Fixed bug when adding employee

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,207 +1,242 @@
 import { useState } from "react"
+import { useFormik } from "formik"
 import {
+    Stack,
     Button,
     TextField,
     Dialog,
-    DialogActions,
-    DialogContent,
     DialogTitle,
+    DialogContent,
 } from "@mui/material"
-import GenderMenu from "./GenderMenu"
-import DepartmentMenu from "./DepartmentMenu"
+import * as yup from "yup"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
 import { query } from "../util/query"
+import DepartmentMenu from "./DepartmentMenu"
 
 export default function AddEmployee({ handleClose, setIsLoading }) {
-    const [empId, setEmpId] = useState("")
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [salary, setSalary] = useState("")
-    const [fromDate, setFromDate] = useState("")
-    const [endDate, setEndDate] = useState("")
-    const [role, setRole] = useState("")
-    const [post, setPost] = useState("")
-    const [dob, setDob] = useState("")
-    const [gender, setGender] = useState("")
     const [deptName, setDeptName] = useState("")
     const [deptId, setDeptId] = useState("")
-
-    const getGender = (gender) => {
-        setGender(gender)
-    }
-
     const getDeptName = (deptName) => {
+        console.error(deptName)
         setDeptName(deptName)
     }
 
     const getDeptId = (deptId) => {
+        console.log(deptId)
         setDeptId(deptId)
     }
 
-    const handleSumbit = async (e) => {
-        setIsLoading(true)
-        const data = {
-            emp_deptId: empId,
-            first_name: firstName,
-            last_name: lastName,
-            salary: salary,
-            from_date: fromDate,
-            to_date: endDate,
-            role: role,
-            post: post,
-            dob: dob,
-            gender: gender,
-            deptName_no: deptId,
-        }
-        e.preventDefault()
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        }
+    const validationSchema = yup.object({
+        firstName: yup.string().required("First name is required"),
+        lastName: yup.string().required("Last name is required"),
+        gender: yup.string().required("Gender is required"),
+        dob: yup.string().required("Dob is required"),
+    })
 
-        try {
-            await query(`/employees`, options)
-
-            handleClose()
-        } catch (e) {
-            console.error(e)
-        } finally {
-            setIsLoading(false)
-        }
-    }
+    const {
+        touched,
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+    } = useFormik({
+        initialValues: {
+            empId: "",
+            firstName: "",
+            lastName: "",
+            gender: "",
+            dob: null,
+            salary: "",
+            fromDate: null,
+            endDate: null,
+            role: "",
+            post: "",
+            // deptName: null,
+            // deptId: null,
+        },
+        validationSchema,
+        onSubmit: async () => {
+            try {
+                setIsLoading(true)
+                const {
+                    empId,
+                    firstName,
+                    lastName,
+                    fromDate,
+                    endDate,
+                    salary,
+                    ...rest
+                } = values
+                console.log(values)
+                await query("employees", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        emp_id: empId,
+                        first_name: firstName,
+                        last_name: lastName,
+                        from_date: fromDate,
+                        to_date: endDate,
+                        salary: Number(salary),
+                        ...rest,
+                        dept_no: deptId,
+                    }),
+                })
+                setIsLoading(false)
+                handleClose()
+            } catch (e) {
+                console.error(e)
+            }
+        },
+    })
 
     return (
         <Dialog open={true} onClose={handleClose}>
             <DialogTitle>Add Employee</DialogTitle>
             <DialogContent>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="empId"
-                    label="Employee ID"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => {
-                        setEmpId(e.target.value)
-                    }}
-                />
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="firstName"
-                    label="First Name"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={firstName}
-                    onChange={(e) => {
-                        setFirstName(e.target.value)
-                    }}
-                />
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="lastName"
-                    label="Last Name"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={lastName}
-                    onChange={(e) => {
-                        setLastName(e.target.value)
-                    }}
-                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Stack
+                        component="form"
+                        onSubmit={handleSubmit}
+                        gap={3}
+                        sx={{ minWidth: "480px", padding: 2 }}
+                    >
+                        <TextField
+                            fullWidth
+                            name="empId"
+                            label="Employee Id"
+                            value={values.empId}
+                            onChange={handleChange}
+                            error={touched.empId && Boolean(errors.empId)}
+                            helperText={touched.empId && errors.empId}
+                        />
+                        <TextField
+                            fullWidth
+                            name="firstName"
+                            label="First Name"
+                            value={values.firstName}
+                            onChange={handleChange}
+                            error={
+                                touched.firstName && Boolean(errors.firstName)
+                            }
+                            helperText={touched.firstName && errors.firstName}
+                        />
+                        <TextField
+                            fullWidth
+                            name="lastName"
+                            label="Last Name"
+                            value={values.lastName}
+                            onChange={handleChange}
+                            error={touched.lastName && Boolean(errors.lastName)}
+                            helperText={touched.lastName && errors.lastName}
+                        />
+                        <TextField
+                            fullWidth
+                            name="gender"
+                            label="Gender"
+                            value={values.gender}
+                            onChange={handleChange}
+                            error={touched.gender && Boolean(errors.gender)}
+                            helperText={touched.gender && errors.gender}
+                        />
+                        <DatePicker
+                            label="DOB"
+                            value={values.dob}
+                            onChange={(value) =>
+                                setFieldValue("dob", value, true)
+                            }
+                            textField={(params) => (
+                                <TextField
+                                    error={Boolean(touched.dob && errors.dob)}
+                                    helperText={touched.dob && errors.dob}
+                                    label="Birthday"
+                                    name="dob"
+                                    fullWidth
+                                    {...params}
+                                />
+                            )}
+                        />
+                        <TextField
+                            fullWidth
+                            name="salary"
+                            label="Salary"
+                            value={values.salary}
+                            onChange={handleChange}
+                            error={touched.salary && Boolean(errors.salary)}
+                            helperText={touched.salary && errors.salary}
+                        />
 
-                <label className="Form" style={{ color: "grey" }}>
-                    DOB:
-                </label>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="dob"
-                    type="date"
-                    fullWidth
-                    variant="standard"
-                    value={dob}
-                    onChange={(e) => {
-                        setDob(e.target.value)
-                    }}
-                />
-                <GenderMenu getGender={getGender} />
-                <DepartmentMenu
-                    getDeptName={getDeptName}
-                    getDeptId={getDeptId}
-                />
+                        <DatePicker
+                            label="From Date"
+                            value={values.fromDate}
+                            onChange={(value) =>
+                                setFieldValue("fromDate", value, true)
+                            }
+                            textField={(params) => (
+                                <TextField
+                                    error={Boolean(touched.dob && errors.dob)}
+                                    helperText={touched.dob && errors.dob}
+                                    label="fromDate"
+                                    name="fromDate"
+                                    fullWidth
+                                    {...params}
+                                />
+                            )}
+                        />
+                        <DatePicker
+                            label="End Date"
+                            value={values.endDate}
+                            onChange={(value) =>
+                                setFieldValue("endDate", value, true)
+                            }
+                            textField={(params) => (
+                                <TextField
+                                    error={Boolean(touched.dob && errors.dob)}
+                                    helperText={touched.dob && errors.dob}
+                                    label="endDate"
+                                    name="endDate"
+                                    fullWidth
+                                    {...params}
+                                />
+                            )}
+                        />
+                        <TextField
+                            fullWidth
+                            name="post"
+                            label="Position"
+                            value={values.post}
+                            onChange={handleChange}
+                            error={touched.post && Boolean(errors.post)}
+                            helperText={touched.post && errors.post}
+                        />
+                        <TextField
+                            fullWidth
+                            name="role"
+                            label="Role"
+                            value={values.role}
+                            onChange={handleChange}
+                            error={touched.role && Boolean(errors.role)}
+                            helperText={touched.role && errors.role}
+                        />
+                        <DepartmentMenu
+                            getDeptName={getDeptName}
+                            getDeptId={getDeptId}
+                        />
 
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="role"
-                    label="Role"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={role}
-                    onChange={(e) => {
-                        setRole(e.target.value)
-                    }}
-                />
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="post"
-                    label="Position"
-                    type="string"
-                    fullWidth
-                    variant="standard"
-                    value={post}
-                    onChange={(e) => {
-                        setPost(e.target.value)
-                    }}
-                />
-
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="salary"
-                    label="Salary"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => setSalary(Number(e.target.value))}
-                />
-                <label className="Form" style={{ color: "grey" }}>
-                    From Date:
-                </label>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="fromDate"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => setFromDate(e.target.value)}
-                />
-                <label className="Form" style={{ color: "grey" }}>
-                    End Date:
-                </label>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="endDate"
-                    type="date"
-                    fullWidth
-                    variant="standard"
-                    onChange={(e) => setEndDate(e.target.value)}
-                />
+                        <Button
+                            color="primary"
+                            variant="outlined"
+                            fullWidth
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
+                    </Stack>
+                </LocalizationProvider>
             </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={async (e) => await handleSumbit(e)}>
-                    Submit
-                </Button>
-            </DialogActions>
         </Dialog>
     )
 }
